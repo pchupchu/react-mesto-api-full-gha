48e69d91ca319856b2e812a0d198cfb6a95f00f3
Marina Malyarchuk <pchupchu@yandex.ru>
2023-05-06 15:52:57 +0300

    try

--- backend/token.js
+++ backend/token.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDU2MjlhMWIzZTJlNTZiMzk4ZTlkYWEiLCJpYXQiOjE2ODMzNjgzNTksImV4cCI6MTY4Mzk3MzE1OX0.PbulK_1V_ZqYVordV5bqHlw4pjaRcJAlE5aeZSur7Vw';
-const SECRET_KEY_DEV = 'super-strong-secret';
+const SECRET_KEY_DEV = 'super-strong';
 
 try {
   const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
